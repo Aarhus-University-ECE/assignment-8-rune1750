b45141507a5f2be92f78fc4101b33353307df151
rune1750 <rune.rune@live.dk>
2022-10-31 22:09:27 +0100

    FINAL

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,9 +1,47 @@
 #include "insertion_sort.h"
 #include "linked_list.h"
 
-
+#include <assert.h>
 
 void sort(linked_list *llPtr)
 {
- // Add your sort function here
+    // placeholder nodes
+    node_t *holder;
+    node_t *holder_next;
+    node_t *last = llPtr->head;
+    node_t *current = last->next;
+    node_t *place = llPtr->head;
+
+    // Only runs the sorter if the list is not empty
+    assert(llPtr->head == NULL);
+
+    // runs untill last node == NULL
+    while (last->next != NULL)
+    {
+        // If current node's data is smaller than the top node, current will be held by a placeholder
+        if (current->data < last->data)
+        {
+            holder = current;
+            holder_next = current->next;
+            last->next = current->next;
+            // Looks for a previous node which has data smaller than the current node
+            while (place->next->data < holder->data)
+            {
+                place = place->next;
+            }
+            if (llPtr->head == place)
+            {
+                holder->next = llPtr->head;
+                llPtr->head = holder;
+            }
+            else
+            {
+                holder->next = place->next;
+                place->next = holder;
+            }
+        }
+    }
+    // sorting starts over, but with top element as the node with the smallest data found
+    last = current;
+    current = last->next;
 }
--- src/list_queue.c
+++ src/list_queue.c
@@ -1,22 +1,72 @@
 #include "list_queue.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
 
 void init_queue(queue *q)
 {
-  // Add your init_queue
+  // Full reset of the queue
+  q->front = NULL;
+  q->rear = NULL;
+  q->size = 0;
 }
 
 int empty(queue *q)
 {
-  // Add your empty function  
+  // If the front of the queue is NULL, the rear of the que will be set to NULL aswell
+  if (q->front == NULL)
+  {
+    q->rear = NULL;
+    printf("\nThe queue has been left abandoned... T_T");
+    return 1;
+  }
+  else
+  {
+    printf("\n There is something in this queue! ^_^");
+    return 0;
+  }
+  // que size reset
+  q->size = 0;
 }
 
 void enqueue(queue *q, int x)
 {
-  // Add your enqueue function
+
+  struct qnode *element = malloc(sizeof(qnode));
+  element->data = x;
+  element->next = NULL;
+
+  // if queue is empty, the inserted element will be both front and rear
+  if (q->front == NULL && q->rear == NULL)
+  {
+    q->rear = element;
+    q->front = element;
+  }
+  else
+  // if queue isnt empty, element will be the rear.
+  {
+    q->rear->next = element;
+    q->rear = element;
+  }
+  // size of the queue is adjusted.
+  q->size++;
 }
 
 int dequeue(queue *q)
 {
-  // Add your dequeue function
+  struct qnode *holder;
+  // If the first place in the queue is empty, the program will print this.
+  if (q->front = NULL)
+  {
+    printf("I dont have anything, leave me alone!");
+  }
+  else
+  {
+    // Front of the queue becomes the second element. qnode "holder" extracts information from queue.
+    holder = q->front;
+    q->front = q->front->next;
+  }
+  // size of the queue is adjusted.
+  q->size--;
+  return holder->data; 
 }
